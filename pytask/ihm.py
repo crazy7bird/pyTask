7659f30e32658072c80b0a7e5a082ec4e3e48a88
ROY Alexandre <AR279990@intra.cea.fr>
2024-10-30 13:41:48 +0100

    [WIP]Adding Term ihm

--- pytask/ihm.py
+++ pytask/ihm.py
@@ -1,4 +1,6 @@
 from os import system, name #know wich os is used.
+import keyboard
+import pytask.KBhit as KBhit
 
 # this module provide terminal interface.
 
@@ -19,11 +21,16 @@ class ihm():
         self.list_size = 0
         self.cursor = 0 # Position of the >
         self.clear_screen()
+        self.kbhit = KBhit.KBHit()
+        #keyboard.on_press_key("up arrow", lambda _: print('mkay'))
+        #keyboard.on_press_key("down arrow", lambda _: print('mkayMouse'))
+
         pass
 
     def datas_update(self, datas : list[str]) -> None :
         self.list = datas
         self.list_size = len(datas)
+        self.update()
     
     def clear_screen(self) -> None :
         # Clear_screen depend on the used OS
@@ -41,14 +48,51 @@ class ihm():
         print("# WIN+ALT+N : new task             #")
         print("####################################")
 
+    def screen_list(self) :
+        for i,el in enumerate(self.list) :
+            if self.cursor == i :
+                print("> ",end="")
+            print(el)
+
+    def up(self):
+        if self.cursor > 0 :
+            self.cursor -= 1
+    def down(self) :
+        if self.cursor < (self.list_size -1) :
+            self.cursor += 1
+
+    def update(self) :
+        self.clear_screen()
+        self.head()
+        self.screen_list()
+        self.tail()
+
+    def manage(self) :
+        update = False
+        if self.kbhit.kbhit():
+            c = self.kbhit.getch()
+            if ord(c) == 72: # UPARROW
+                self.up()
+                update = True
+            elif ord(c) == 80: # DOWNARROW
+                self.down()
+                update = True
+
+            if update :
+                self.update()
+
+    def __del__(self) :
+        self.kbhit.set_normal_term()
 
 
 def main() :
+    data = ["One", "Deux", "Tress"]
     IHM = ihm()
-    
-    IHM.head()
-    print(">")
-    IHM.tail()
+    IHM.datas_update(data)
+    IHM.manage()
+    while True :
+        IHM.manage()
+        continue
     return
 
 if __name__ == "__main__" :
--- pytask/key.py
+++ pytask/key.py
@@ -3,29 +3,26 @@ import time
 
 import pytask.pop_up as pop_up
 import pytask.task_list as task_list
+import pytask.ihm as ihm
 
 
 
 def main () -> None :
     b = task_list.backlog()
     w = pop_up.window()
-
-    t = 0
+    ihmm = ihm.ihm()
+    ihmm.datas_update(b.list_backlog())
 
     while True :
-        time.sleep(0.1) # Cooldown a litle
+        time.sleep(0.02) # Cooldown a litle
         if w.user_start :
             w.start()
         
         if w.user_end :
             ret = w.stop()
-            print(f"Final Entry : {ret}")
             b.add_task(ret)
             b.save_backlog()
-
+            ihmm.datas_update(b.list_backlog())
 
         w.update()
-
-        if time.time() > t + 4 :
-            print(f"Entry : {b.list_backlog()}")
-            t = time.time()
+        ihmm.manage()
